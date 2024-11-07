adf40572b62355ef414b4bdeae721178b2c5fca7
Almir Hodžić <81625008+hdzcalmir@users.noreply.github.com>
2024-11-07 12:47:47 +0100

    updated to aurelia beta 22 and iau2config removed

--- packages/01_common/src/index.ts
+++ packages/01_common/src/index.ts
@@ -2,6 +2,5 @@ export * from './timeProvider';
 export * from './waiter';
 export * from './queue';
 export * from './cancellationToken';
-export * from './au2config';
 export * from './looper';
 export * from './configuration';
