c80963009bfd8493ae1e67ca4f99f872400054a0
Almir Hodžić <81625008+hdzcalmir@users.noreply.github.com>
2024-11-07 22:47:33 +0100

    update

--- packages/01_common/src/configuration.ts
+++ packages/01_common/src/configuration.ts
@@ -2,6 +2,7 @@ import { IContainer, Registration } from '@aurelia/kernel';
 import { IWaiter, Waiter } from './waiter';
 import { ILooper, Looper } from './looper';
 import { ITimeProvider, TimeProvider } from './timeProvider';
+import { AU2Config, IAU2Config } from 'au2config';
 
 export const StarnetCommonsPlugin = create();
 
@@ -11,7 +12,8 @@ function create() {
 			return container.register(
 				Registration.transient(IWaiter, Waiter),
 				Registration.transient(ILooper, Looper),
-				Registration.transient(ITimeProvider, TimeProvider)
+				Registration.transient(ITimeProvider, TimeProvider),
+				Registration.singleton(IAU2Config, AU2Config)
 			);
 		}
 	};
